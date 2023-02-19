cda86b53050f75c7b7bfbef2925310c12fd43c52
Sikandar Saleem <sikandarsaleem2000@gmail.com>
2023-02-19 12:17:44 +0500

    add links in app

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default function Blogs(){
   return (
     <>
-      <h1>Blog 1</h1>
-      <h1>Blog 2</h1>
-      <h1>Blog 3</h1>
-      <h1>Blog 4</h1>
+      <h1><Link href="blogs/1">Blog 1</Link></h1>
+      <h1><Link href="blogs/2">Blog 2</Link></h1>
+      <h1><Link href="blogs/3" replace>Blog 3</Link></h1>
+
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Home(){
+  const router = useRouter();
+  const clickHandler = () => {
+    alert('Your order is placed and we moving to blogs');
+    router.push('/blogs');
+    console.log(router)
+  }
   return(
     <>
       <h1>This is home page</h1>
+      <Link href="/blogs">Blogs</Link><br />
+      <Link href="/docs">Docs</Link>
+      <button onClick={clickHandler}>Place Order</button>
     </>
   )
 }
